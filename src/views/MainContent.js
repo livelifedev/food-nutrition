04990ad917e39438756ad5d002e106b4d6657469
John <johnrubio93@gmail.com>
2019-09-27 23:07:16 +1000

    added hooks and handlers for changing day

--- src/views/MainContent.js
+++ src/views/MainContent.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Profile from "../views/main_content/Profile";
 import Diet from "../views/main_content/Diet";
 
 const MainContent = () => {
+  const [day, setDay] = useState(0);
   return (
     <div className="main-content">
-      <Profile />
+      <Profile
+        day={day}
+        handleNext={() => setDay(day + 1)}
+        handleBack={() => setDay(day - 1)}
+      />
       <Diet />
     </div>
   );
--- src/views/main_content/Profile.js
+++ src/views/main_content/Profile.js
@@ -7,11 +7,10 @@ import diet from "../../data/mock-data";
 // Profile is just displaying information, so logic may not be necessary in here, extract later
 // Data (diet/intake) can be pulled in the parent, then values passed to here
 
-const Profile = () => {
+const Profile = ({ day, handleNext, handleBack }) => {
   const { data_points } = diet;
   console.log(data_points);
 
-  let day = 1;
   let mealMap = data_points[day].intake_list.reduce((acc, current) => {
     if (!acc[current.meal_type]) {
       acc[current.meal_type] = current.nf_calories;
@@ -29,28 +28,31 @@ const Profile = () => {
   console.log(mealMap);
 
   return (
-    <section className="profile">
-      <h1>Profile</h1>
-      <div className="profile-summary">
-        <p>
-          {diet.first_name} {diet.last_name}
-        </p>
-        <p>{diet.height_cm}cm</p>
-        <p>{diet.weight_kg}kg</p>
-      </div>
+    <>
+      <button onClick={handleNext}>Click</button>
+      <section className="profile">
+        <h1>Profile</h1>
+        <div className="profile-summary">
+          <p>
+            {diet.first_name} {diet.last_name}
+          </p>
+          <p>{diet.height_cm}cm</p>
+          <p>{diet.weight_kg}kg</p>
+        </div>
 
-      <div className="profile-details">
-        <p>{mealMap.total} cal consumed</p>
-        <p>{diet.daily_goal} daily goal</p>
-      </div>
+        <div className="profile-details">
+          <p>{mealMap.total} cal consumed</p>
+          <p>{diet.daily_goal} daily goal</p>
+        </div>
 
-      <div className="profile-log">
-        <p>{mealMap.breakfast} cal Breakfast</p>
-        <p>{mealMap.lunch} cal Lunch</p>
-        <p>{mealMap.dinner} cal Dinner</p>
-        <p>{mealMap.snack} cal Snack</p>
-      </div>
-    </section>
+        <div className="profile-log">
+          <p>{mealMap.breakfast} cal Breakfast</p>
+          <p>{mealMap.lunch} cal Lunch</p>
+          <p>{mealMap.dinner} cal Dinner</p>
+          <p>{mealMap.snack} cal Snack</p>
+        </div>
+      </section>
+    </>
   );
 };
 
