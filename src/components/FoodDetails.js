f3bb4bb1c9b1194b24ffd7903a3b997d30796e05
John <johnrubio93@gmail.com>
2019-10-05 11:40:16 +1000

    started implementation of food details when list item clicked

---
+++ src/components/FoodDetails.js
@@ -0,0 +1,7 @@
+const FoodDetails = () => {
+  // servings need to be adjustable
+  // grams and calories get recalculated to servings
+  // load item photo, item.photo render to pageß
+};
+
+export default FoodDetails;
--- src/components/FoodDisplay.js
+++ src/components/FoodDisplay.js
@@ -1,12 +1,20 @@
 import React from "react";
 
 const FoodDisplay = ({ food }) => {
+  const openFoodDetails = food => {
+    console.log("click", food);
+  };
+
   return (
     <>
       <ul>
         {food.map(item => {
           const id = `id-${item.food_name.replace(" ", "-")}`;
-          return <li key={id}>{item.food_name}</li>;
+          return (
+            <li key={id} onClick={() => openFoodDetails(item.food_name)}>
+              {item.food_name}
+            </li>
+          );
         })}
       </ul>
     </>
